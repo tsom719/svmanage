74e744a5eeb0a6118b5b0294504c8e70be6170d6
tsom719 <jayden212@naver.com>
2023-02-14 20:07:32 +0900

    기존 기능별 파일 분리 > 파일 통합 | thanks, @ㅎㅎ from kakao talk

--- events/buttonInteraction.js
+++ events/buttonInteraction.js
@@ -21,7 +21,7 @@ module.exports = {
 		const { client } = interaction;
 
 		// Checks if the interaction is a button interaction (to prevent weird bugs)
-		
+
 		if (!interaction.isButton()) return;
 
 		const command = client.buttonCommands.get(interaction.customId);
@@ -29,23 +29,51 @@ module.exports = {
 		// If the interaction is not a command in cache, return error message.
 		// You can modify the error message at ./messages/defaultButtonError.js file!
 
-		if (!command) {
-			await require("../messages/defaultButtonError").execute(interaction);
-			return;
-		}
-
 		// A try to execute the interaction.
-
-		try {
-			await command.execute(interaction);
-			return;
-		} catch (err) {
-			console.error(err);
-			await interaction.reply({
-				content: "There was an issue while executing that button!",
-				ephemeral: true,
-			});
-			return;
+		if (
+			interaction.customId.startsWith("restartit") ||
+			interaction.customId.startsWith("stopit") ||
+			interaction.customId.startsWith("statusit")
+		) {
+			try {
+				await client.buttonCommands.get("controlapps").execute(interaction);
+				return;
+			} catch (err) {
+				console.error(err);
+				await interaction.reply({
+					content: "There was an issue while executing that button!",
+					ephemeral: true,
+				});
+				return;
+			}
+		} else if (interaction.customId.endsWith("entry")) {
+			try {
+				await client.buttonCommands.get("serviceentry").execute(interaction);
+				return;
+			} catch (err) {
+				console.error(err);
+				await interaction.reply({
+					content: "There was an issue while executing that button!",
+					ephemeral: true,
+				});
+				return;
+			}
+		} else {
+			if (!command) {
+				await require("../messages/defaultButtonError").execute(interaction);
+				return;
+			}
+			try {
+				await command.execute(interaction);
+				return;
+			} catch (err) {
+				console.error(err);
+				await interaction.reply({
+					content: "There was an issue while executing that button!",
+					ephemeral: true,
+				});
+				return;
+			}
 		}
 	},
 };
---
+++ interactions/buttons/menu/serviceentry.js
@@ -0,0 +1,77 @@
+const {
+	Client,
+	Collection,
+	GatewayIntentBits,
+	Partials,
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	Events,
+	EmbedBuilder,
+	SlashCommandBuilder,
+} = require("discord.js");
+const functoko = {
+	restart: "재시작",
+	status: "정보 보기",
+	stop: "중지",
+};
+const functoit = {
+	restartentry: "restart",
+	statusentry: "status",
+	stopentry: "stop",
+};
+module.exports = {
+	id: "serviceentry",
+
+	async execute(interaction) {
+		let funcit = functoit[interaction.customId];
+
+		if (
+			interaction.member.roles.cache.some(
+				(r) => r.id == "1074241811314389022"
+			) ||
+			funcit == "status"
+		) {
+			const { NodeSSH } = require("node-ssh");
+			const ssh = new NodeSSH();
+			const row = new ActionRowBuilder()
+				.addComponents(
+					new ButtonBuilder()
+						.setCustomId(`${funcit}itseaheaven`)
+						.setLabel("seaheaven")
+						.setStyle(ButtonStyle.Primary)
+				)
+				.addComponents(
+					new ButtonBuilder()
+						.setCustomId(`${funcit}itapache2`)
+						.setLabel("apache2")
+						.setStyle(ButtonStyle.Primary)
+				)
+				.addComponents(
+					new ButtonBuilder()
+						.setCustomId(`${funcit}itmtbot`)
+						.setLabel("mtbot")
+						.setStyle(ButtonStyle.Primary)
+				)
+				.addComponents(
+					new ButtonBuilder()
+						.setCustomId(`${funcit}itcodesjbot`)
+						.setLabel("codesjbot")
+						.setStyle(ButtonStyle.Primary)
+				);
+
+			await interaction.reply({
+				content: `Codesj 서비스 ${functoko[funcit]}`,
+				ephemeral: true,
+				components: [row],
+			});
+		} else {
+			await interaction.reply({
+				content: "권한이 없습니다. 관리자에게 요청하세요",
+				ephemeral: true,
+			});
+		}
+
+		return;
+	},
+};
